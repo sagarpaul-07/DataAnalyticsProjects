3f6bfb7dd3d514d869a0735bdadaaeabf17e0ba8
Sagar <sagarpaul07@gmail.com>
2026-08-20 00:48:33 +0530

    Delete SQLQueryEngagementdata.sql

--- SQLQueryEngagementdata.sql
+++
@@ -1,9 +0,0 @@
-USE PortfolioProject_MarketingAnalytics;
-GO
-
-IF COL_LENGTH('dbo.engagement_data', 'Views') IS NULL
-    ALTER TABLE dbo.engagement_data ADD Views INT;
-
-IF COL_LENGTH('dbo.engagement_data', 'Clicks') IS NULL
-    ALTER TABLE dbo.engagement_data ADD Clicks INT;
-GO
